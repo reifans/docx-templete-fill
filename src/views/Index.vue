fc572c1731765ceea9452c72d8687d26cff2a9a9
chengjiahui <327127960@qq.com>
2022-09-21 14:06:11 +0800

    init push

---
+++ src/views/Index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="btn" @click="print">按钮</div>
+</template>
+
+<script>
+import { exportDocx } from '@/utils/docx.js'
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {
+      print() {
+          exportDocx('/templete.docx', {userName: '鸡汤'}, '模版.docx')
+      }
+  },
+  created() {},
+  mounted() {},
+};
+</script>
+
+<style  scoped>
+.btn {
+    width: 50px;
+    height: 50px;
+    background-color: pink;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    lintOnSave: false, // 当保存时不进行eslint的检查
+    devServer:{
+        open:false//自动打开默认浏览器
+    }
+}
